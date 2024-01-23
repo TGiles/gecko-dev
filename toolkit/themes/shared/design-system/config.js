6fd4e686f45c89c9ac0f3844733eb9e7515645c5
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2024-01-23 14:45:20 -0500

    Remove build.js, use css/variables formatter, update tokens structure
    This removes the build.js file in favor of using config.js (which
    requires `style-dictionary build` to build our files). I also removed
    the custom file formatting code in favor of the included css/variables
    formatter. Lastly, I updated the tokens structure so instead of
    "darkValue", we use "dark".
    
    Most likely the structure of the tokens file will change again, but
    I'll create a new PR with those proposed changes.

--- toolkit/themes/shared/design-system/config.js
+++ toolkit/themes/shared/design-system/config.js
@@ -14,36 +14,36 @@ const { fileHeader } = StyleDictionary.formatHelpers;
  */
 function lightDarkFormatter(args) {
   let dictionary = args.dictionary;
-  let file = args.file;
-  const selector = args.options.selector ? args.options.selector : `:root`;
-  const indentation = args.options.indentation ? args.options.indentation: 2;
   let modifiedDictionary = dictionary.allTokens.map(token => {
-    let value = JSON.stringify(token.value);
     // the `dictionary` object now has `usesReference()` and
     // `getReferences()` methods. `usesReference()` will return true if
     // the value has a reference in it. `getReferences()` will return
     // an array of references to the whole tokens so that you can access their
     // names or any other attributes.
-    if (dictionary.usesReference(token.original.value) && token.original.darkValue) {
+    if (token.original.dark) {
       // Note: make sure to use `token.original.value` because
       // `token.value` is already resolved at this point.
       const lightRefs = dictionary.getReferences(token.original.value);
-      const darkRefs = dictionary.getReferences(token.original.darkValue);
-      for (let i = 0; i < darkRefs.length; i++) {
-        let currentLight = lightRefs[i].name;
-        let currentDark = darkRefs[i].name;
-        value = `light-dark(var(--${currentLight}), var(--${currentDark}))`;
+      const darkRefs = dictionary.getReferences(token.original.dark);
+      if (lightRefs.length > 0 || darkRefs.length > 0) {
+        for (let i = 0; i < darkRefs.length; i++) {
+          let currentLightName = lightRefs[i]?.name;
+          let currentDarkName = darkRefs[i]?.name;
+          let lightValue = currentLightName ? 
+          `var(--${currentLightName})` : `${token.original.value}`;
+          let darkValue = currentDarkName ?
+          `var(--${currentDarkName})` : `${token.original.dark}`;
+          token.value = `light-dark(${lightValue}, ${darkValue})`
+        }
+      } else {
+        value = `light-dark(${token.original.value}, ${token.original.dark})`;
         token.value = value;
       }
     }
     return token;
   });
   dictionary.allTokens = modifiedDictionary;
-  let mappedValues = modifiedDictionary.map(element => {
-    let joinedValue = `${" ".repeat(indentation)}--${element.name}: ${element.value};`
-    return joinedValue;
-  }).join("\n");
-  return `${fileHeader({ file })}\n${selector} {\n${mappedValues}\n}`;
+  return StyleDictionary.format["css/variables"]({...args, dictionary});
 }
 
 module.exports = {
@@ -58,6 +58,9 @@ module.exports = {
       files: [{
         destination: "light-dark.css",
         format: "css/variables/light-dark",
+        options: {
+          outputReferences: true,
+        }
       }]
     }
   }
