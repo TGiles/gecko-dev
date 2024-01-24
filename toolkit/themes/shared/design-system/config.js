1c43410159aa3b66572a9e84a841ba8c3aebb48d
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2024-01-24 11:22:23 -0500

    Remove light-dark formatter and replace with transformer.
    We can greatly simplify the config.js file by doing two things:
    1. Use a transformer for dealing with light-dark.
    2. In this transformer, modify the original value of the token.
    
    By modifying the value of the original token, we are able to take
    advantage of the built-in css/variables formatting and so we don't need
    custom CSS file formatting.
    
    Additionally, we can use ...StyleDictionary.transformGroup["group_name"]
    to get the individual transforms of a group. By doing this we can extend
    an existing group as seen in this patch.

--- toolkit/themes/shared/design-system/config.js
+++ toolkit/themes/shared/design-system/config.js
@@ -5,69 +5,39 @@
 /* eslint-env node */
 
 const StyleDictionary = require("style-dictionary");
-const { fileHeader, sortByReference } = StyleDictionary.formatHelpers;
-
-/**
- * CSS formatter that converts tokens that have a light and dark value
- * to a light-dark() function. If we try to use the built-in CSS formatter,
- * we will lose the "dark" value or any other property that isn't "value".
- */
-function lightDarkFormatter(args) {
-  let dictionary = args.dictionary;
-  let indentation = args.indentation ? args.indentation : "  ";
-  let file = args.file;
-  let selector = args.selector ? args.selector : `:root`;
-  // Sort references after their definition
-  dictionary.allTokens = [...dictionary.allTokens].sort(sortByReference(dictionary));
-  let modifiedDictionary = dictionary.allTokens.map(token => {
-    // the `dictionary` object now has `usesReference()` and
-    // `getReferences()` methods. `usesReference()` will return true if
-    // the value has a reference in it. `getReferences()` will return
-    // an array of references to the whole tokens so that you can access their
-    // names or any other attributes.
-    if (token.original.dark) {
-      // Note: make sure to use `token.original.value` because
-      // `token.value` is already resolved at this point.
-      const lightRefs = dictionary.getReferences(token.original.value);
-      const darkRefs = dictionary.getReferences(token.original.dark);
-      let currentLightName = lightRefs[0]?.name;
-      let currentDarkName = darkRefs[0]?.name;
-      let lightValue = currentLightName ?
-        `var(--${currentLightName})` : `${token.original.value}`;
-      let darkValue = currentDarkName ?
-        `var(--${currentDarkName})` : `${token.original.dark}`;
-      token.value = `light-dark(${lightValue}, ${darkValue})`
-    }
-    return token;
-  });
-  dictionary.allTokens = modifiedDictionary;
-
-  // We have to manually format the generated tokens into the file.
-  // If we try to use the formattedVariables() helper with the CSS format
-  // and outputReferences: true, then we will lose our light-dark() function.
-  let mappedValues = modifiedDictionary.map(element => {
-    let joinedValue = `${indentation}--${element.name}: ${element.value};`
-    return joinedValue;
-  }).join("\n");
-  return `${fileHeader({ file })}\n${selector} {\n${mappedValues}\n}`;
-}
 
 module.exports = {
   source: ["design-tokens.json"],
-  format: {
-    "css/variables/light-dark": lightDarkFormatter,
+  transform: {
+    lightDarkTransform: {
+      type: "value",
+      transitive: true,
+      name: "lightDarkTransform",
+      matcher: token => token.original.value && token.original.dark,
+      transformer: token => {
+        let lightDarkValue = `light-dark(${token.original.value}, ${token.original.dark})`;
+        // modify the original value and everything works like magic
+        token.original.value = lightDarkValue;
+        return lightDarkValue;
+      },
+    },
   },
   platforms: {
     css: {
-      transformGroup: "css",
+      transforms: [
+        ...StyleDictionary.transformGroup["css"],
+        "lightDarkTransform",
+      ],
       buildPath: "build/css/",
-      files: [{
-        destination: "light-dark.css",
-        format: "css/variables/light-dark",
-        options: {
-          outputReferences: true,
-        }
-      }]
-    }
-  }
-}
+      files: [
+        {
+          destination: "light-dark.css",
+          format: "css/variables",
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+  },
+};
