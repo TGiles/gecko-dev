410d97e3e917bfcee29958b9003b4d918b76d41a
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2024-01-24 15:02:53 -0500

    fix eslint error

--- toolkit/themes/shared/design-system/config.js
+++ toolkit/themes/shared/design-system/config.js
@@ -25,7 +25,7 @@ module.exports = {
   platforms: {
     css: {
       transforms: [
-        ...StyleDictionary.transformGroup["css"],
+        ...StyleDictionary.transformGroup.css,
         "lightDarkTransform",
       ],
       buildPath: "build/css/",
